8785467a3d8aaa55af09dfae959dfc3ca5a9ea8d
alex <alex@Blue-pos>
2016-02-17 19:29:07 -0600

    register and login functions Completed

--- routes/index.js
+++ routes/index.js
@@ -2,37 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Clique' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Clique'});
 });
 
 /* GET Userlist stuff page displays the usercollection for testing purposes. */
-router.get('/userlist', function(req, res) {
+router.get('/userlist', function (req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
+    collection.find({}, {}, function (e, docs) {
         res.render('userlist', {
-            "userlist" : docs
+            "userlist": docs
         });
     });
 });
 
 // GET regTest.jade the REGISTRATION TEST PAGE. 
-router.get('/regTest', function(req, res) {
-    res.render('regTest', { title: 'Simulates a User Registration to /register' });
+router.get('/regTest', function (req, res) {
+    res.render('regTest', {title: 'Simulates a User Registration to /register'});
 });
 
 // GET loginTest.jade the LOGIN TEST PAGE. 
-router.get('/loginTest', function(req, res) {
-    res.render('loginTest', { title: 'Simulates a User Login to /login' });
+router.get('/loginTest', function (req, res) {
+    res.render('loginTest', {title: 'Simulates a User Login to /login'});
 });
 
 // POST JSON data to the User Registration function
-router.post('/register', function(req, res) {
+router.post('/register', function (req, res) {
 
     // Set the JSON data to a variable
     var str = req.body;
-    
+
     // Set the internal DB variable
     var db = req.db;
 
@@ -41,23 +41,23 @@ router.post('/register', function(req, res) {
 
     // Submit to the DB, adding a new user object to usercollection
     collection.insert({
-        "username" : str.username,
-        "email" : str.email,
-        "password" : str.password,
-        "fullname" : str.fullname
+        "username": str.username,
+        "email": str.email,
+        "password": str.password,
+        "fullname": str.fullname
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
-        console.log("There was a problem adding the information to the database.");   
-        res.send({'message':"Fail"});
-        }
-        else {
+            console.log("There was a problem adding the information to the database.");
+            res.send({'message': err});
+        } else {
             // And forward to success page 
             /* this is where a modification can be 
              made to return json data to the client app */
-        res.send({'message':"Success"});    
-        //res.redirect("userlist"); // allows for success confirmation on the server side. to be removed later
-       }
+            console.log(doc);
+            res.send(doc);
+            //res.redirect("userlist"); // allows for success confirmation on the server side. to be removed later
+        }
     });
 });
 
@@ -66,25 +66,37 @@ router.post('/login', function (req, res) {
 
     // Set the JSON data to a variable
     var str = req.body;
-    
     // Set the internal DB variable
     var db = req.db;
     // Obtains the user profile collection setting it to a variable
     var collection = db.get('usercollection');
-    
-    // Get our JSON values.
-    var userName = req.body.username;
-    var userPass = req.body.password;
-    
+
     // Finds a single document in the collection
-    db.usercollection.findOne({ username: userName }).on('success', function (doc) {
-        // the password is incorrect
-        if (userPass !== doc.password){
-            res.send({'message':"Fail"});
+    collection.findOne({username: req.body.username}, function (err, doc) {
+        if (err) {
+            console.log("There was a problem adding the information to the database.");
+            res.send({'message': err});
         }
-        // the password matches!
+        // Found a username match in databse
+        if (doc) {
+            var urlPass = req.body.password;
+            var dbPass = doc.password;
+            // the password matches!
+            if (urlPass == dbPass) {
+                console.log(dbPass + " matches " + urlPass);
+                // Send user profile in JSON
+                res.send(doc);
+            }
+            // The password is incorrect
+            else {
+                console.log(dbPass + " does not match " + urlPass);
+                res.send({'message': "Fail"});
+            }
+        }
+        // No username match found in database
         else {
-            res.send({'message':"Success"});
+            console.log(req.body.password + " does not exist");
+            res.send({'message': "user does not exist"});
         }
     });
 });
